29f6d152eff795900fe1fba033f2259b26f788f6
Dysphie <dysphie@gmail.com>
2019-03-20 16:02:19 -0300

    Spaghetti, better go back one commit

--- whodisquiz.py
+++ whodisquiz.py
@@ -11,10 +11,23 @@ import discord
 import asyncio
 import os
 from discord.ext import commands
+import argparse
+import logging
+import random
+from typing import List
+
+from pymongo import MongoClient, database
 
 bot = commands.Bot(command_prefix='.')
 
 auth_token = os.getenv('DISCORD_BOT_TOKEN')
+
+MESSAGE_BLACKLIST = ['html', 'bot c', 'botc']
+WORD_BLACKLIST = ['http://', 'https://', '[ ]']
+USER_BLACKLIST = ['kanqaroo', 'Olex', 'rocket.cat', 'bot', 'AmhpLiFy', 'Z',
+                  'WCEendT', 'Foofoo14', 'Glass', 'testaccount', 'jack.lupino', 'SylphCA',
+                  'jorainbo', 'x00irt', 'slamarechi', 'tadiconedr', 'Z', 'Bowser',
+                  'el_frotal', 'Conifacio', 'Uwaai', 'Reign', '-_-BIER--_--', 'Nicarasu']
 
 quiz_active = False
 quiz_submissions = {}
@@ -27,12 +40,11 @@ async def whodis(ctx):
     quiz_active = True
 
     await ctx.message.add_reaction('⏳')
-    model_user = pick_random_user()
-    fake_msg = fabricate_msg(model_user)
+    model_user, fake_msg = fabricate_msg()
     
     await ctx.send('**Who Dis Quiz:** \n %s' % fake_msg)
     await ctx.message.clear_reactions()
-    await asyncio.sleep(15)
+    await asyncio.sleep(20)
 
     winners = []
     for player, guess in quiz_submissions.items():
@@ -41,7 +53,7 @@ async def whodis(ctx):
 
     winner_count = len(winners)
     outcome = "None"
-    if winner_count:
+    if (winner_count > 0):
         outcome = winners[0] if winner_count == 1 else ', '.join(winners[:-1]) + ", and " + winners[-1]
 
     await ctx.send('The answer was **%s**! Winners: %s' % (model_user, outcome))
@@ -56,12 +68,110 @@ async def on_message(message):
         if author not in quiz_submissions.keys():
             quiz_submissions[author.mention] = message.content
 
-def fabricate_msg(username):
-    # TODO
-    return 'Lorem ipsum dolor sit amet'
+
+def get_db(host:str, port:int, database:str):
+    db_client = MongoClient(host, port)
+    return db_client.get_database(database)
+
+
+def get_words(messages_collection: database.Collection, query_filter: dict) -> List[str]:
+    unique_words = []
+    query = messages_collection.find(query_filter, {'msg': 1})
+    for obj in query:
+        msg = obj.get('msg')
+        if not msg or should_skip_message(msg):
+            continue
+        words = msg.split()
+        if not words or len(words) > 20:
+            continue
+        unique_words.extend([word for word in words if should_include_word(word)])
+    return unique_words
+
+
+def should_skip_message(msg: str) -> bool:
+    lower_msg = msg.lower()
+    return any(word in lower_msg for word in MESSAGE_BLACKLIST)
+
+
+def should_include_word(word: str) -> bool:
+    lower_word = word.lower()
+    return not any(word in lower_word for word in WORD_BLACKLIST)
+
+
+def should_skip_user(user: str) -> bool:
+    lower_user = user.lower()
+    return any(word in lower_user for word in USER_BLACKLIST)
+
+
+def make_pairs(words: List[str]):
+    for i in range(len(words) - 1):
+        yield words[i], words[i + 1]
+
+
+def get_all_words(all_words: List[str]) -> List[str]:
+    pairs = make_pairs(all_words)
+    word_dict = {}
+
+    for word_1, word_2 in pairs:
+        if word_1 in word_dict:
+            word_dict[word_1].append(word_2)
+        else:
+            word_dict[word_1] = [word_2]
+
+    first_word = random.choice(all_words)
+
+    while first_word.islower():
+        first_word = random.choice(all_words)
+
+    chain = [first_word]
+
+    for i in range(80):
+        last_word = chain[-1]
+        if last_word in word_dict:
+            chain.append(random.choice(word_dict[last_word]))
+    return chain
+
 
 def pick_random_user():
-    # TODO
-    return 'Q'
+    candidates = []
 
-bot.run(auth_token)
+    users = db.get_collection('users')
+    query = users.find({ "name": { '$exists': True } })
+    for obj in query:
+        user = obj.get('username')
+        if not user or should_skip_user(user):
+            continue
+        candidates.append(user)
+   
+    return random.choice(candidates)
+
+
+def fabricate_msg():
+
+    db = get_db('localhost', 27017, 'rocketchat')
+
+    candidates = []
+    users = db.get_collection('users')
+    query = users.find({ "name": { '$exists': True } })
+    for obj in query:
+        user = obj.get('username')
+        if not user or should_skip_user(user):
+            continue
+        candidates.append(user)
+   
+    username = random.choice(candidates)
+
+    query_filter = {}
+    if username:
+        user = users.find_one({'username': username})
+        if not user:
+            raise Exception('User not found: %s. User the --list-users flag to list all users.' % username)
+        query_filter['u._id'] = user['_id']
+    messages_collection = db.get_collection('rocketchat_message')
+    all_words = get_words(messages_collection, query_filter)
+    words = get_all_words(all_words)
+    return username, ' '.join(words)
+
+
+bot.run("NDAwMDkyNDA5ODM0NTA0MjEy.D3OEYw.gXHIZNCr3sgfoxEHVdnZqlUpMVg")
+
