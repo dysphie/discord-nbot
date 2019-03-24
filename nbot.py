7ed4589843e827fe0157f1bd08b8d45e25d9318f
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2019-03-24 20:24:18 +0100

    restructure into files

--- nbot.py
+++ nbot.py
@@ -1,92 +1,34 @@
-import discord
-from discord.ext import commands
-from pymongo import MongoClient
-import markovify
-import re
+import logging
 import os
 
-BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-DATABASE_URI = os.getenv('DATABASE_URI') # mongodb://localhost:27017/
+import discord
+from discord.ext import commands
 
-if not BOT_TOKEN:
-    raise Exception('You must set the DISCORD_BOT_TOKEN environment variable')
+from markov_generator import fabricate_sentence, create_model
 
-if not DATABASE_URI:
-    raise Exception('You must set the DATABASE_URI environment variable')      
-
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(asctime)s - %(message)s')
 bot = commands.Bot(command_prefix='.')
+
+BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
 
 @bot.event
 async def on_ready():
-    print('Ready')
-
-db = MongoClient(DATABASE_URI)['nbot_brain']
-chat = db['discord_chat']
-models = db['model_cache']
+    logging.info('Ready')
 
 
-class DiscordText(markovify.NewlineText):
-
-    # github.com/jsvine/markovify/issues/84
-    def test_sentence_input(self, sentence):
-        return True
-
-
-def create_model(user_id):
-
-    print('Creating speech model for %d...' % user_id)
-
-    query = chat.find({'user_id': user_id}, {'message': 1})
-    # TODO: Improve this query, make it ignore null AND empty strings
-
-    if query is None:
-        print('Didnt find user')
-        return None
-
-    reject_pattern = re.compile(r"(^')|('$)|\s'|'\s|[\"(\(\)\[\])]") 
-    # Discard orphaned brackets and quotes
-
-    total = query.count()
-    parsed = 0
-
-    # This approach is slow as shit, but generates compact models and is easy on memory
-    model = None
-    for document in query:
-        parsed += 1
-        print('Processing entry %d of %d' % (parsed, total))
-        message = document.get('message')
-        if message == '' or re.search(reject_pattern, message): continue
-
-        model_chunk = DiscordText(message)
-        if model:
-            model = markovify.combine(models=[model, model_chunk])
-        else:
-            model = model_chunk
-        
-    return model
-
-
-def fabricate_sentence(user_id):
-
-    # Get cached model or bail
-    query = models.find_one({'user_id': user_id})
-    if query is None: return
-        
-    model = DiscordText.from_json(query.get('model')) 
-    return model.make_sentence(tries=100)
-
-    
 @bot.command()
-async def be(ctx, *args):
-
+async def be(ctx: discord.Webhook, *args) -> object:
     guild = ctx.channel.guild
     nickname = ' '.join(args)
 
     user = guild.get_member_named(nickname)
-    if not user: return
+    if not user:
+        return
 
     content = fabricate_sentence(user.id)
 
+    # todo webhook id from env vars
     webhook = await ctx.channel.create_webhook(name='Temporary')
     await webhook.send(content=content, username=nickname, avatar_url=user.avatar_url)
     await webhook.delete()
@@ -94,12 +36,10 @@ async def be(ctx, *args):
 
 @bot.command()
 async def add(ctx, user_id: int):
-
     model = create_model(user_id)
-    models.insert_one({'user_id': user_id, 'model': model.to_json()})
 
     for i in range(5):
-        print('TEST SENTENCE: ' + model.make_sentence(tries=100))
+        logging.debug('TEST SENTENCE: ' + model.make_sentence(tries=100))
 
 
 bot.run(BOT_TOKEN)
