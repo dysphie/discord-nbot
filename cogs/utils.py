120670c271a45f20b30b510b2f2ae9b51dc59be8
Dysphie <dysphie@gmail.com>
2020-06-16 03:55:49 -0300

    Refactor stuff

---
+++ cogs/utils.py
@@ -0,0 +1,26 @@
+from discord import TextChannel
+from discord.ext import commands
+
+
+class Utils(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.bot_has_permissions(manage_webhooks=True)
+    async def get_webhook_for_channel(self, channel: TextChannel):
+        webhooks = await channel.webhooks()
+        try:
+            webhook = next(w for w in webhooks if w.user == self.bot.user)
+        except StopIteration:
+            webhook = await channel.create_webhook(name='NBot')
+
+        return webhook
+
+    @staticmethod
+    def truncate_string(s: str, maxlen: int, suffix='..'):
+        return s[:maxlen - len(suffix)] + suffix if len(s) > maxlen else s
+
+
+def setup(bot):
+    bot.add_cog(Utils(bot))
