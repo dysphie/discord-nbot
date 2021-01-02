e4750ba39040b59caed85232c71949bd5cec8693
Dysphie <dysphie@gmail.com>
2021-01-01 22:16:21 -0300

    Add inspirational quoting

---
+++ cogs/inspire.py
@@ -0,0 +1,22 @@
+from io import BytesIO
+import discord
+from discord.ext import commands
+
+class Inspire(commands.Cog, name="Inspire"):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = bot.session
+
+    @commands.command()
+    async def inspire(self, ctx):
+        async with self.session.get('https://inspirobot.me/api?generate=true') as r:
+            if r.status != 200:
+                return await ctx.error('Could not fetch file...')
+
+            data = BytesIO(await r.read())
+            await ctx.send(file=discord.File(data, 'inspiration.png'))
+
+
+def setup(bot):
+    bot.add_cog(Inspire(bot))
