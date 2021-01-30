0b2266e5e251a6efb23ec91ad41e838143e50d8b
dysphie <dysphie@gmail.com>
2021-01-29 21:12:51 -0300

    Changes from master

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -1,8 +1,5 @@
-import textwrap
-from discord import RawReactionActionEvent, Message, Embed, utils, NotFound, Forbidden, HTTPException
+from discord import Embed
 from discord.ext import commands
-
-STAR_EMOJI = '⭐'
 
 
 class Starboard(commands.Cog):
@@ -15,74 +12,47 @@ class Starboard(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         self.starchannel = self.bot.get_channel(self.bot.cfg['starboard_channel'])
-        if not self.starchannel:
-            raise Exception('Invalid starboard channel')
 
     @commands.Cog.listener()
     @commands.guild_only()
-    async def on_raw_reaction_add(self, payload: RawReactionActionEvent):
-        await self.on_raw_reaction_event(payload)
+    async def on_raw_reaction_add(self, payload):
 
-    @commands.Cog.listener()
-    @commands.guild_only()
-    async def on_raw_reaction_remove(self, payload: RawReactionActionEvent):
-        await self.on_raw_reaction_event(payload)
-
-    async def on_raw_reaction_event(self, payload: RawReactionActionEvent):
-
-        if str(payload.emoji) != STAR_EMOJI:
+        if self.starchannel is None or str(payload.emoji) != '⭐':
             return
 
         channel = await self.bot.fetch_channel(payload.channel_id)
-        original = await channel.fetch_message(payload.message_id)
 
-        if original.author.id == payload.user_id:  # No self starring!
+        starred_data = await self.stardb.find_one({'_id': payload.message_id})
+        if starred_data:
+            starred = await self.starchannel.fetch_message(starred_data['star_id'])
+            if starred:
+                await self.increase_reactions(starred)
             return
 
-        already_starred = await self.stardb.find_one({'_id': payload.message_id})
-        if already_starred:
-            try:
-                starred = await self.starchannel.fetch_message(already_starred['star_id'])
-            except (NotFound, Forbidden, HTTPException):
-                pass
-            else:
-                await self.update_starred(starred, original)
-        else:
-            await self.star_message(original)
+        message = await channel.fetch_message(payload.message_id)
+        if message:
+            await self.star_message(message)
 
-    async def star_message(self, original: Message):
+    async def star_message(self, message):
 
-        # Find suitable webhook transport
-        starred = await self.starchannel.send(
-            reference=original,
-            mention_author=False,
-            content=self.generate_star_stats(original)
-        )
+        embed = Embed(description=message.content)
+        embed.set_author(name='Content', url=message.jump_url)
+        embed.add_field(name='Author', value=message.author.mention)
+        embed.add_field(name='Channel', value=message.channel.mention)
+        embed.set_footer(text='1 gay bean')
+        starred = await self.starchannel.send(embed=embed)
 
-        if starred:
-            await self.stardb.insert_one({'_id': original.id, 'star_id': starred.id})
-
-    async def update_starred(self, starred: Message, original: Message):
-
-        num_stars = self.count_stars(original)
-        print(num_stars)
-        if num_stars <= 0:
-            await starred.delete()
-            await self.stardb.delete_one({'_id': original.id})
-        else:
-            new_content = self.generate_star_stats(original, stars_override=num_stars)
-            await starred.edit(content=new_content)
-
-    def generate_star_stats(self, message: Message, stars_override=None) -> str:
-        num_stars = stars_override or self.count_stars(message)
-        return f'⭐ **{num_stars}**'
+        await self.stardb.insert_one({'_id': message.id, 'star_id': starred.id})
 
     @staticmethod
-    def count_stars(message: Message):
-        for reaction in message.reactions:
-            if reaction.emoji == STAR_EMOJI:
-                return reaction.count
-        return 0
+    async def increase_reactions(starred):
+        embedict = starred.embeds[0].copy().to_dict()
+        print(embedict)
+        embedict['footer']['text'] = 'many gay bean'
+        embed = Embed.from_dict(embedict)
+        await starred.edit(embed=embed)
+
+    # TODO: reaction remove
 
 
 def setup(bot):
