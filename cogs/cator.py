0d106ea8de02360630f73659aa2d242f81d9f65c
Dysphie <dysphie@gmail.com>
2022-01-11 13:07:05 -0300

    Fix git status

--- cogs/cator.py
+++ cogs/cator.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 
 class Cator(commands.Cog, name="Cator"):
 
-    url = 'https://thesecatsdonotexist.com'
+    url = 'https://d2ph5fj80uercy.cloudfront.net'
 
     def __init__(self, bot):
         self.bot = bot
@@ -16,14 +16,15 @@ class Cator(commands.Cog, name="Cator"):
 
     @slash_command(name="cat", description='Posts an AI generated picture of a cat')
     async def cat(self, ctx):
-
-        folder = choice(['04', '05', '06'])
+        folder = randint(1, 6)
         catnum = randint(0, 5000)
-
-        cat_url = f'https://d2ph5fj80uercy.cloudfront.net/{folder}/cat{catnum}.jpg'
+        cat_url = f'{self.url}/0{folder}/cat{catnum}.jpg'
+        print(cat_url)
         async with self.session.get(cat_url) as resp:
             data = io.BytesIO(await resp.read())
-            await ctx.send(file=discord.File(data, 'cat.png'))
+
+            # use 'files' because 'file' throws an exception (bug?)
+            await ctx.respond(files=[discord.File(data, 'cat.png')])
 
 
 def setup(bot):
